71aac6f3f0781d475759c3da00a785bc99c24695
Prashanth Sriram <prashanthsriram10@gmail.com>
2023-03-12 12:15:54 +0530

    Added Task Addition Fn

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import Footer from "./footer";
 import { useState } from "react";
 function Tasks(){
   const [tasks, setTasks] = useState([]);
+  const [text, setText] = useState('');
+
+  function handleAdd(){
+    setTasks([...tasks, {val:text, done:false}]);
+    setText('');
+  }
+
+  return(
+    <>
+    <div className="addTask">
+      <input value={text} onChange={(e)=>setText(e.target.value)}/>  
+      <button onClick={handleAdd}>Add</button>
+    </div>
+    <div className="taskList">
+      <ul>
+      {tasks.map((t, i)=><li key={i}>{t.val}</li>)}
+      </ul>
+    </div>
+    </>
+  )
 }
 function App() {
   return (
