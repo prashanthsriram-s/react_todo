835123f7aca3b931f71ee6e3de22c296ba60c4fc
Prashanth Sriram <prashanthsriram10@gmail.com>
2023-03-12 12:04:09 +0530

    Made Skeleton of components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-
+import Header from "./header";
+import Footer from "./footer";
+import { useState } from "react";
+function Tasks(){
+  const [tasks, setTasks] = useState([]);
+}
 function App() {
   return (
-    <h1>Hello</h1>
+    <>
+    <Header />
+    <Tasks />
+    <Footer />
+    </>
   );
 }
 
---
+++ src/footer.js
@@ -0,0 +1,5 @@
+export default function Footer(){
+    return (
+        <h1 className="footer1">Footer goes here</h1>
+    );
+}
